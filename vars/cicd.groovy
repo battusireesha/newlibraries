769170113231adc1a596a194486b25ed39b334fa
battusireesha <140335026+battusireesha@users.noreply.github.com>
2023-08-03 17:05:20 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,22 @@
+def gitdwnld(repo)
+{
+  git "https://github.com/intelliqittrainings/${repo}.git"
+}
 
+def mavenbuild()
+{
+  sh 'mvnpackage'
+}
+
+def tomcatdeploy(jobname,ip,context)
+{
+
+
+}
+
+def runselenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}.testing.jar"
+}
+
+  
