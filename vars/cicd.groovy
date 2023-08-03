bc6495c6a58d5d3faa31de824bf7ca76f5a1e068
battusireesha <140335026+battusireesha@users.noreply.github.com>
2023-08-03 17:42:22 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,7 +11,7 @@ def mavenbuild()
 def tomcatdeploy(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
-            }
+            
 }
 
 def runselenium(jobname)
